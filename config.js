a614fa3fd0ebb504f3cef71da12ff79be08b5233
gxus00a <138877503+gxus00a@users.noreply.github.com>
2023-07-07 13:50:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 //inserisci il tuo o i tuoi numeri 
 
 global.owner = [
-  ['393518398856', 'nobody|']
+  [+1 (817) 566-2386', 'nobody|']
 
 ] //inserisci il tuo o i tuoi numeri 
 
